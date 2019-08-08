1b4c6b053a638e782a8bd467abcfa186f4a97ec2
yhidad31 <yasminehmahmoud@gmail.com>
2019-08-08 10:34:10 -0700

    adding fold for project

---
+++ Event-Recommender-System/event-reco-syst.js
@@ -0,0 +1,32 @@
+class EventRecommender() {
+  constructor() {
+  // All main properties should go here.
+  
+  this.events = [];
+  this.users = [];
+  
+  }
+
+  addEvent() {
+  // Adds a new Event to the System
+  }
+
+  addUser() {
+  // Adds a new User to the System
+  }
+
+  saveUserEvent(){
+  //Allow users to save events to a personal Events array.
+  }
+
+  deleteUser() {
+  // Deletes a User from the system
+  }
+ 
+  deleteEvent() {
+  // Deletes the Event from the system
+  }
+
+  filter(){
+  }
+}
