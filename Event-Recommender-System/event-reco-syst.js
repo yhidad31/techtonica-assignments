95237a65e23d07294c24b06e036c28c5f6f807b2
yhidad31 <yasminehmahmoud@gmail.com>
2019-08-08 19:23:52 -0700

    committing changes

--- Event-Recommender-System/event-reco-syst.js
+++ Event-Recommender-System/event-reco-syst.js
@@ -1,4 +1,24 @@
-class EventRecommender() {
+class Events{
+  constructor(eventName, eventType){
+    this.eventName = eventName;
+    this.eventType = eventType;
+    this.eventDate = eventDate;
+    this.eventPrice = eventPrice;
+  }
+}
+class Users{
+  constructor(userName,userInterests){
+    this.userName = userName;
+    this.userInterests = userInterests;
+    this.userSavedEvents = [];
+  }
+  // userRegina.addEvent("Warriors Game");
+  addEvent(chosenEvent){
+    this.userSavedEvents.push(chosenEvent);
+  }
+}
+
+class EventRecommender{
   constructor() {
   // All main properties should go here.
   this.events = [];
@@ -8,18 +28,24 @@ class EventRecommender() {
 
   addEvent() {
   // Adds a new Event to the System
+  let newEvent = new Event (eventName,eventType,eventDate,eventPrice);
+  this.events.push(newEvent);
   }
 
-  addUser() {
+  addUser(user) {
   // Adds a new User to the System
+  this.users.push(user);
+
   }
 
   saveUserEvent(){
   //Allow users to save events to a personal Events array.
   }
 
-  deleteUser() {
+  deleteUser(userName) {
   // Deletes a User from the system
+    
+  
   }
  
   deleteEvent() {
@@ -29,19 +55,28 @@ class EventRecommender() {
   filter(){
   }
 }
-class Events(){
-  constructor(eventName, eventTYpe){
-    this.eventName = eventName;
-    this.eventType = eventType;
-    this.eventDate = eventDate;
-    this.eventPrice = eventPrice;
-  }
-}
-class Users (){
-  constructor(userName,userInterests){
-    this.userName = userName;
-    this.userInterests = userInterests;
-    this.userSavedEvents = userSavedEvents;
-  }
+//below instantiate class -- creating object of that class
 
-}
+
+//let my varibable called system be an instance of the class EventRecommender
+
+// When you instantiate an EventRecommender object, you should be able to do the following:
+let system = new EventRecommender();
+// Users:
+// Add a new User to the System
+let userYasmine = new Users("Yasmine","movies");
+let userRegina = new Users("Regina","sports");
+system.addUser(userYasmine);
+system.addUser(userRegina);
+console.log(system);
+// Allow users to save events to a personal Events array
+userRegina.addEvent("Warriors Game");
+userYasmine.addEvent("Harry Potter");
+console.log(userRegina);
+console.log(userYasmine);
+// Delete a User from the System
+// Event:
+// Add a new Event to the System
+// Delete an Event from the System
+// Filter:
+// Note: You can use two separate filter functions or one if you'd like an extra challenge :)
