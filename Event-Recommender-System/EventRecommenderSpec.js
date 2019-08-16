b632076b9bfe63c8f96d85e05e701e9013ba7315
yhidad31 <yasminehmahmoud@gmail.com>
2019-08-16 11:01:20 -0700

    final version 1.0

---
+++ Event-Recommender-System/EventRecommenderSpec.js
@@ -0,0 +1,47 @@
+describe("EventRecommender", () => {
+  const EventRecommender = require("../event-reco-syst.js");
+  let er;
+  beforeEach(() => {
+    er = new EventRecommender();
+  });
+
+  describe("addEvent", () => {
+    it("adds a new Event to the system", () => {
+      er.addEvent("Change Me");
+      // expect(er.events.length).toEqual(1);
+      // expect(er.events[0].title).toEqual("Change Me"); // what are some other things you can test?
+    });
+  });
+  
+  describe("addUser", () => {
+    it("adds a new User to the system", () => {
+      er.addUser("Change Me");
+      // expect(er.user.length).toEqual(1);
+    });
+  });
+    
+  // describe("addEvent", () => {
+  //   it("adds an event to a user's personal event array", () => {
+  //     er.addEvent("Make a new event");
+  //     er.addUser("Make a new user");
+  //     er.saveUserEvent("event", "user"); // change these to match your method signature
+  //     expect(er.user.personalEvents.length).toEqual(1);
+  //   });
+  // });
+    
+  describe("deleteUser", () => {
+    it("removes a User from the system", () => {
+      er.addUser("Yasmine");
+      er.deleteUser("Yasmine");
+      // expect(er.user.length).toEqual(0);
+    });
+  });
+    
+  describe("deleteEvent", () => {
+    it("removes the event from the system", () => {
+      er.addEvent("Harry Potter 1");
+      er.deleteEvent("Harry Potter 1");
+      // expect(er.events.length).toEqual(0);
+    });
+  });
+});
--- Event-Recommender-System/spec/event-reco-syst.js
+++ Event-Recommender-System/spec/event-reco-syst.js
@@ -12,7 +12,6 @@ class Users{
     this.userInterests = userInterests;
     this.userSavedEvents = [];
   }
-  // userRegina.addEvent("Warriors Game");
   addEvent(chosenEvent){
     this.userSavedEvents.push(chosenEvent);
   }
@@ -26,7 +25,7 @@ class EventRecommender{
   
   }
 
-  addEvent(anEvent) {
+  addEvent(newEvent) {
   // Adds a new Event to the System
   this.events.push(newEvent);
   }
@@ -37,23 +36,11 @@ class EventRecommender{
 
   }
 
-  saveUserEvent(){
-  //Allow users to save events to a personal Events array.
-  }
-
   deleteUser(userToDelete) {
   // Deletes a User from the system
   //1.Find the array element w userName
-  this.users = [userYasmine, userRegina]
   //iterate thru array element w username to match 'each element'.userName == userName
-  // let userIndex = this.users.indexOf(userName);
-  //   this.user.splice(userIndex,1);
-//   for (let i = 0; i < this.users.length; i++){
-//   if (this.users[i].userName === userName) {
-//     this.users.splice([i],1)
-//   }
-// }
-this.users.filter(user => user.userName != userToDelete);
+this.users = this.users.filter(users => users.userName != userToDelete);
 // this.users.filter(function(user){
 //   user.userName != userName;
 // }
@@ -61,29 +48,29 @@ this.users.filter(user => user.userName != userToDelete);
   
   }
  
-  deleteEvent() {
+  deleteEvent(eventToDelete) {
   // Deletes the Event from the system
+  this.events = this.events.filter(events => events.eventName != eventToDelete);
   }
 //input to filter?
 //output to filter?
-this.users.filter(user => user.userName != userToDelete);
-  filter(){
-    searchTickets(min,max){
-      let msg = "Eligible tickets: ";
+
+    filter(min,max){
+      let msg = "Available events: ";
       let counter = 1;
-      for (let i =0; i<this.availableTickets.length; i++){
+      for (let i =0; i<this.eventPrice.length; i++){
         if (this.availableTickets[i].ticketPrice >= min && this.availableTickets[i].ticketPrice<=max){
           msg += counter + "."+ " " + this.availableTickets[i].ticketType + " ";
           counter++;
       }
      } 
      if (counter === 1){
-       msg = "There are no available tickets.";
+       msg = "There are relevant events.";
      }
       return msg;
      }
   }
-}
+
 //below instantiate class -- creating object of that class
 
 
@@ -98,35 +85,36 @@ let userRegina = new Users("Regina","sports");
 let userKesha = new Users("Kesha","gardening");
 system.addUser(userYasmine);
 system.addUser(userRegina);
+system.addUser(userKesha);
 console.log(system);
 // Allow users to save events to a personal Events array
-userRegina.addEvent("Warriors Game");
-userYasmine.addEvent("Harry Potter");
-console.log(userRegina);
-console.log(userYasmine);
-// Delete a User from the System
-system.deleteUser("Yasmine");
-// Event:
-// Add a new Event to the System
-let eventWarriorsVsSpurs = new Event("Warriors vs. Spurs", "Sports","August 5",50);
+let eventWarriorsVsSpurs = new Events("Warriors vs. Spurs", "Sports","August 5",50);
 system.addEvent(eventWarriorsVsSpurs);
-let eventPastaCookingClass = new Event("Learn to make buccatini from scratch","Cooking","August 7",50);
-system.addEvent(eventCookingClass);
-let eventHarryPotterSocererStone = new Event("Harry Potter 1", "Movie","August 7",15);
+let eventPastaCookingClass = new Events("Learn to make buccatini from scratch","Cooking","August 7",50);
+system.addEvent(eventPastaCookingClass);
+let eventHarryPotterSocererStone = new Events("Harry Potter 1", "Movie","August 7",15);
 system.addEvent(eventHarryPotterSocererStone);
-let eventGardeningForFall = new Event("Learn to plant and grow winter squash","Gardening","August 10",20);
+let eventGardeningForFall = new Events("Learn to plant and grow winter squash","Gardening","August 10",20);
 system.addEvent(eventGardeningForFall);
 
-//Allow user to add event to personal array
-userYasmine.addFavoriteEvent(eventHarryPotterSocererStone);
-userRegina.addFavoriteEvent(eventWarriorsVsSpurs);
-userKesha.addFavoriteEvent(eventWarriorsVsSpurs);
-userKesha.addFavoriteEvent(eventGardeningForFall);
+userYasmine.addEvent(eventHarryPotterSocererStone);
+userRegina.addEvent(eventWarriorsVsSpurs);
+userKesha.addEvent(eventWarriorsVsSpurs);
+userKesha.addEvent(eventGardeningForFall);
 console.log(userYasmine);
 console.log(userRegina);
-console.log(userYasmine);
+console.log(userKesha);
+
+// Delete a User from the System
+system.deleteUser("Yasmine");
+
+
+//Allow user to add event to personal array
+
 // Delete an Event from the System
-system.deleteEvent(eventHarryPotterSocererStone);
+system.deleteEvent("Harry Potter 1");
 // Filter:
 // Note: You can use two separate filter functions or one if you'd like an extra challenge :)
+console.log(system);
 
+module.exports = EventRecommender;
