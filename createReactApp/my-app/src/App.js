83e3a04d2b6351c2b50602edf5c794c33f096559
yhidad31 <yasminehmahmoud@gmail.com>
2019-09-03 21:08:07 -0700

    starting react app

---
+++ createReactApp/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component { render(){
+
+
+  return (React.createElement(
+    'div',
+    null,
+    'Welcome to React',
+    )
+  );
+}
+}
+export default App;
