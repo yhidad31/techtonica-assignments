83bd4e5b907f7e3601306c6e6f56b04870c5b30e
yhidad31 <yasminehmahmoud@gmail.com>
2019-10-28 11:03:50 -0700

    beginning to add  auth0

--- HealthTrac/react-frontend/src/App.js
+++ HealthTrac/react-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./App.css";
 
 export default class App extends Component {
   constructor(props) {
@@ -9,29 +10,60 @@ export default class App extends Component {
       players: [],
       player: {}
     };
-}
-componentDidMount() {
-  fetch("http://localhost:3001/players/list")
-  .then(response => response.json())
-  .then( responseJson=> {
-    this.setState({ players:responseJson.data });
-  },
-)}
-showCard=id=> {
-  fetch(`http://localhost:3001/players/${id}`)
-  .then(response => response.json())
-  .then(
-  responseJson=> {this.setState({ player:responseJson.data })},
-);
-this.setState({
-  list:false,
-  card:true
-});
-};
-showList = () => {
-   this.setState({
-     card: false,
-     list: true
-   });
-};
+  }
+  componentDidMount() {
+    fetch("http://localhost:3001/players/list")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ players: responseJson.data });
+      });
+  }
+  showCard = id => {
+    fetch(`http://localhost:3001/players/${id}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ player: responseJson.data });
+      });
+    this.setState({
+      list: false,
+      card: true
+    });
+  };
+
+  showList = () => {
+    this.setState({
+      card: false,
+      list: true
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.list ? (
+          <div className="list-group">
+            {this.state.players.map(player => (
+              <li
+                onClick={() => this.showCard(player._id)}
+                className="list-group-item list-group-item-action"
+              >
+                {player.name}
+              </li>
+            ))}
+          </div>
+        ) : null}
+        {this.state.card ? (
+          <div class="card" style={{ width: "18rem" }}>
+            <div class="card-body">
+              <h5 class="card-title">{this.state.player.name}</h5>
+              <p class="card-text">{this.state.player.runs}</p>
+              <div onClick={() => this.showList()} class="btn btn-primary">
+                Back
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
