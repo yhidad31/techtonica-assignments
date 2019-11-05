118d06d3e046f15e60bde019a511390ccc90d9c6
yhidad31 <yasminehmahmoud@gmail.com>
2019-11-05 15:24:02 -0800

    adding new get requests

--- HealthTrac/crud/HealthTrac-backend/int.js
+++
@@ -1,9 +0,0 @@
-function madLibs(str){
-  let newArr = str.split(' ');
- for (let i = 0; i < newArr.length; i++){
-   newArr[i] = newArr[i].replace('__noun__', 'dog')
-  } 
-  return newArr.join(' ');
- }
-
- console.log(madLibs('Her nice __noun__ ran to the pond'));
