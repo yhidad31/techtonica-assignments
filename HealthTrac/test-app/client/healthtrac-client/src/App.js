4dbe3af5930c4c4194178eacbfc34a4c5083e35d
yhidad31 <yasminehmahmoud@gmail.com>
2019-11-18 11:44:24 -0800

    troubleshooting auth

--- HealthTrac/test-app/client/healthtrac-client/src/App.js
+++ HealthTrac/test-app/client/healthtrac-client/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import ExternalApi from "./components/ExternalApi";
+import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ HealthTrac/test-app/client/healthtrac-client/src/components/ExternalApi.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { useAuth0 } from "../react-auth0-spa";
+
+const ExternalApi = () => {
+  const [showResult, setShowResult] = React.useState(false);
+  const [apiMessage, setApiMessage] = React.useState("");
+  const { getTokenSilently } = useAuth0();
+
+  const callApi = async () => {
+    try {
+      const token = await getTokenSilently();
+
+      const response = await fetch("/api/external", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      const responseData = await response.json();
+
+      setShowResult(true);
+      setApiMessage(responseData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>External API</h1>
+      <button onClick={callApi}>Ping API</button>
+      {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+    </>
+  );
+};
+
+export default ExternalApi;
