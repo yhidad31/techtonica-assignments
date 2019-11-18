406d9a604a120179ec5dd5e6c15252f440f00f86
yhidad31 <yasminehmahmoud@gmail.com>
2019-11-18 13:56:09 -0800

    0auth functional

--- HealthTrac/test-app/client/healthtrac-client/src/index.js
+++ HealthTrac/test-app/client/healthtrac-client/src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import config from "./auth_config.json";
+import { Auth0Provider } from "./react-auth0-spa";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// A function that routes the user to the right place after login
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    audience={config.audience}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
